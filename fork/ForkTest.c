029984a4922d3d09a3cc26725afa295774944761
Luke Roche <LukeRocheDev@gmail.com>
2022-08-14 08:38:57 -0400

    job performing forks

--- fork/ForkTest.c
+++ fork/ForkTest.c
@@ -4,32 +4,54 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+
+int job2_permute_10(int len){
+    if(len <= 0){
+        printf("\n");
+        return 0;
+    }
+    printf("1");
+    job2_permute_10(len-1);
+    printf("0");
+    job2_permute_10(len-1);
+    printf("*");
+    return 0;
+}
+int job1_calc_fib(int n){
+    if(n <= 2) return n;
+    int a = job1_calc_fib(n - 1);
+    int b = job1_calc_fib(n - 2);
+    printf("%d\n", a + b);
+    return a + b;
+}
 int main(void){
     pid_t pid;
     int rv;
     int sum = 0;
-    const int CHILD_PROC = 5;
-    int arr[CHILD_PROC];
-    for(int i = 0; i < CHILD_PROC; i++){
+    const int JOBS = 2;
+    int(*jobs[JOBS])(int);
+    jobs[0] = job1_calc_fib;
+    jobs[1] = job2_permute_10;
+
+    for(int i = 0; i < JOBS; i++){
         switch(pid = fork()) {
-        case -1:
-        perror("fork failed"); /* something went wrong */
-        exit(1); /* parent exits */
-        case 0:
-        printf(" CHILD: PID is %d\n", getpid());
-        printf(" CHILD: PPID is %d\n", getppid());
-        printf(" CHILD: Enter exit status...\n");
-        scanf(" %d", &rv);
-        printf(" CHILD: Done!\n");
-        exit(rv);
-        default:
-        printf("PARENT: PID is %d\n", getpid());
-        printf("PARENT: Child PID is %d\n", pid);
-        printf("PARENT: I'm now waiting for my child to exit()...\n");
-        wait(&rv);
-        arr[i] = WEXITSTATUS(rv);
-        printf("PARENT: My child's exit status is: %d\n", WEXITSTATUS(rv));
-        printf("PARENT: Done!\n");
+            case -1:
+                perror("fork failed"); /* something went wrong */
+                exit(1); /* parent exits */
+            case 0:
+                printf(" CHILD: PID is %d\n", getpid());
+                printf(" CHILD: PPID is %d\n", getppid());
+                printf(" CHILD: running child\n");
+                rv = jobs[i](5);
+                printf(" CHILD: Done!\n");
+                exit(rv);
+            default:
+                printf("PARENT: PID is %d\n", getpid());
+                printf("PARENT: Child PID is %d\n", pid);
+                printf("PARENT: I'm now waiting for my child to exit()...\n");
+                wait(&rv);
+                printf("PARENT: My child's exit status is: %d\n", WEXITSTATUS(rv));
+                printf("PARENT: Done!\n");
         }
     }
     printf("Final: %d\n", sum);
